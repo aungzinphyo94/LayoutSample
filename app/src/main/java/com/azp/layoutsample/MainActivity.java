dfa895574aad1252f0a25ee53f5ad7a108dfaa72
Aung Zin Phyo <aungzinphyo94@gmail.com>
2019-04-28 11:10:54 +0630

    Linear layout and Relative layout

---
+++ app/src/main/java/com/azp/layoutsample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.azp.layoutsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relative_layout);
+
+    }
+}
